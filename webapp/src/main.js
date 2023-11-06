31607e23ff3ed0597a76d468a18e2cc9ed7abeae
Me-Cht <2225660185@qq.com>
2023-11-06 22:54:35 +0800

    删除server及无关组件

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,10 +1,8 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/store'; // 导入 store
-import doubounce from "@/utils/doubounce";
 import ElementUI from 'element-plus'
 const app = createApp(App)
 
